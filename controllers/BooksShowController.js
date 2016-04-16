82f568f584836c6f8aabaf2c2b3f30e46a0f7fee
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-04-15 18:03:01 -0700

    got update to work

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -5,6 +5,7 @@ BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
 
+//DISPLAY SINGLE BOOK IN PAGE
   $http({
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/books/'+ $routeParams.bookId
@@ -14,4 +15,29 @@ function BooksShowController($http, $routeParams, $location) {
   }, function onError(response) {
     console.log('ERRR on ID response', response);
   });
+
+//UPDATE SINGLE BOOK
+  vm.editBook = function (book) {
+    console.log(book);
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/'+ book._id,
+      data: book
+    }).then(function onSuccess(response) {
+      $location.path('/');
+      console.log(response);
+    }, function onError(response) {
+      console.log('ERRRRR on update', response);
+    });
+
+};
+
+// //DELETE BOOK
+//   vm.deleteBook = function (book) {
+//     $http({
+//       method: 'DELETE',
+//       url: 'https://super-crud.herokuapp.com/books/' + book._id
+//     })
+  // }
+
 }
